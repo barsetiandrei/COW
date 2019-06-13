293c1a7b0827dc88bf68c99023f40df196c74ab4
barsetiandrei <yo_blondu_12@yahoo.com>
2019-06-13 04:56:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Cal-O-Web</title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+	<div class="title">
+	<h1>Welcome to Cal-O-Web</h1>
+	<h2> Healthy eating and exercise for life!</h2>
+	  <div class="button">
+	    <a href="/pages/home.php" class="btn">Start here</a>
+      </div>
+	</div>
+	
+</body>
+</html>
+ 
